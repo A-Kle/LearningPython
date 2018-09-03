9dfad0d927b42b65f005f89b50b1f799a6249cae
unknown <agnieszka.klebusz@gmail.com>
2018-09-03 04:48:34 +0200

    RPG app sample

--- attack/attack.py
+++ attack/attack.py
@@ -1,6 +1,28 @@
 import random
 
 
+class Enemy:
+    hp = 200
+
+    def __init__(self, atkl, atkh): #constructor
+        self.atkl = atkl
+        self.atkh = atkh
+
+    def getAtk(self):
+        print(self.atkl)
+
+    def getHp(self):
+        print("hp is ", self.hp)
+
+enemy1 = Enemy(75, 90) #creating instance (not using new in python)
+enemy1.getAtk()
+enemy1.getHp()
+
+enemy2 = Enemy(40, 49)
+enemy2.getAtk()
+enemy2.getHp()
+"""
+
 playerhp = 290
 enemyatkl = 60
 enemyatkh = 80
@@ -16,4 +38,4 @@ while playerhp > 0:
         print("You are dead.")
     
 
-    
+    """
